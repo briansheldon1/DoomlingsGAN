74d1e85e038300352f67cafe1500574acd9b351d
Brian Sheldon <briansouthpas@gmail.com>
2024-10-12 15:38:56 -0700

    implementing pytorch transform for converting tensor->PIL before plotting

--- src/gen_images.py
+++ src/gen_images.py
@@ -1,48 +1,67 @@
 import torch
 from PIL import Image
-import numpy as np
+from torchvision import transforms
 import os
 
 from models import Generator
+from util import adjust_contrast_based_on_brightness, normalize_noise
 
 ''' File for generating images using generator '''
 
 
-def generate_images_main(num_images, save_path, gen_model_path = "../saved_models/gen.pkl", 
-                         noise_size=100, ex_realimg_path = "../traits_resized/acrobatic.png"):
+def generate_images_main(num_images, save_path, 
+                        gen_model_path = "../saved_models/gen.pkl",
+                        noise_size=128, 
+                        ex_realimg_path = "../traits_resized/acrobatic.png"):
+    ''' 
+        Main function for generating and saving images using a pretrained model
+
+        Arguments:
+        - num_images: number of images to generate
+        - save_path: path to save the generated images
+        - gen_model_path: path to the pretrained generator model 
+        - noise_size: size of the noise vector
+        - ex_realimg_path: path to an ex of an image that model was trained 
+                           on for dimensionality purposes
+    '''
 
 
-    # open example real img to get flat image size and dimensions
-    dir_path = os.path.dirname(os.path.abspath(__file__))
-    img_path = os.path.join(dir_path, ex_realimg_path)
-    ex_img = Image.open(img_path)
+    # open example real img to get shape
+    ex_realimg_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), ex_realimg_path)
+    ex_img = Image.open(ex_realimg_path)
     img_width, img_height = ex_img.size
-    flat_img_size = img_width*img_height*3 # 3 for rgb
-
+    img_shape = [3, img_height, img_width]
 
     # load model
-    generator = Generator(flat_img_size, noise_size=noise_size)
+    generator = Generator(img_shape, noise_size=noise_size)
     generator.load(gen_model_path)
 
     # generate images
-    noise = torch.randn(num_images, noise_size)
+    noise = torch.randn(num_images, noise_size, 1, 1)
+    noise = normalize_noise(noise)
     gen_imgs = generator(noise)
 
     # reshape images to go back to PIL
-    save_dir = os.path.join(dir_path, save_path)
+    tens_to_pil = transforms.ToPILImage()
+    save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), save_path)
     for i, img_tensor in enumerate(gen_imgs):
 
-        img_reshaped = img_tensor.view(img_height, img_width, 3)
-        img_np = img_reshaped.detach().cpu().numpy()
-        img_np = (img_np*255).astype(np.uint8)
-        img_pil = Image.fromarray(img_np)
+        # reshape
+        img_t = img_tensor.reshape(*img_shape)
 
-        img_pil.save(f"{save_dir}/gen_img_{i}.png")
+        # convert to pil and save
+        img_pil = tens_to_pil(img_t)
+        img_pil = adjust_contrast_based_on_brightness(img_pil)
+
+        # resize image so easier to see
+        img_pil = img_pil.resize((img_pil.size[0]*2, img_pil.size[1]*2))
+        img_pil.save(f"{save_path}/img{i}.png")
 
 
 if __name__ == '__main__':
-    main_args = {
-        'num_images': 5,
+
+    gen_images_args = {
+        'num_images': 20,
         'save_path': "../generated_imgs"
     }
-    generate_images_main(**main_args)
+    generate_images_main(**gen_images_args)
